b39e6bceefa27ab01e4c76b5d74126969a7d599b
PRABHU PRASAD PENTHOI <72146816+PRABHU-OFFICIAL@users.noreply.github.com>
2023-02-20 20:26:07 +0530

    Search Bar Created

--- lib/course_page_resources/course_page.dart
+++ lib/course_page_resources/course_page.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
 
   @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  Icon customIcon = const Icon(Icons.search);
+  Widget customSearchBar = const Text(
+    "Explore Courses",
+    style: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Course Page")),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: customSearchBar,
+        iconTheme: const IconThemeData(color: Colors.black),
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  if (customIcon.icon == Icons.search) {
+                    customIcon = const Icon(Icons.cancel);
+                    customSearchBar = const ListTile(
+                      leading: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                        size: 25,
+                      ),
+                      title: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'type in course name...',
+                          hintStyle: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 25,
+                            fontStyle: FontStyle.italic,
+                          ),
+                          border: InputBorder.none,
+                        ),
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  } else {
+                    customIcon = const Icon(Icons.search);
+                    customSearchBar = const Text(
+                      "Explore Courses",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  }
+                });
+              },
+              icon: customIcon)
+        ],
+      ),
+      body: const Center(child: Text("Course Page")),
     );
   }
 }
